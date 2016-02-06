df778742cda22c06ba0b5fbaf24cea98936507cb
Florian Rappl <rappl.florian@gmail.com>
2016-02-07 00:00:04 +0100

    Changed script component

--- Sumerics.Scripts/ScriptControl.xaml.cs
+++ Sumerics.Scripts/ScriptControl.xaml.cs
@@ -1,28 +1,20 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Collections.ObjectModel;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
-using YAMP;
-
-namespace Sumerics.Controls
+﻿namespace Sumerics.Controls
 {
+    using System;
+    using System.Collections.Generic;
+    using System.Collections.ObjectModel;
+    using System.Windows;
+    using System.Windows.Controls;
+    using System.Windows.Input;
+    using System.Windows.Media;
+    using YAMP;
+
 	/// <summary>
 	/// Interaction logic for ScriptControl.xaml
 	/// </summary>
 	public partial class ScriptControl : UserControl
 	{
-		#region Binding stuff
+		#region Dependency Properties
 
 		public static readonly DependencyProperty SettingsCommandProperty = DependencyProperty.Register(
 			"SettingsCommand",
@@ -40,7 +32,7 @@ namespace Sumerics.Controls
 			"Context", 
 			typeof(ParseContext),
 			typeof(ScriptControl), 
-			new PropertyMetadata(Parser.PrimaryContext));
+			new PropertyMetadata(null));
 
 		public static readonly DependencyProperty SensorFunctionsProperty = DependencyProperty.Register(
 			"SensorFunctions",
@@ -82,20 +74,21 @@ namespace Sumerics.Controls
 
 		#region Constants
 
-		const int EL_OFFSET = 5;
-		const int EL_HEIGHT = 50;
+		const Int32 ElementOffset = 5;
+		const Int32 ElementHeight = 50;
 
 		#endregion
 
-		#region Members
+        #region Fields
 
-		double scale = 1.0;
-		double translateX = 0.0;
-		double translateY = 0.0;
-		List<OutConnectorNode> inputNodes = new List<OutConnectorNode>();
-		List<InConnectorNode> outputNodes = new List<InConnectorNode>();
+        readonly List<OutConnectorNode> _inputNodes = new List<OutConnectorNode>();
+        readonly List<InConnectorNode> _outputNodes = new List<InConnectorNode>();
 
-        static ScriptControl instance;
+		Double _scale = 1.0;
+		Double _translateX = 0.0;
+		Double _translateY = 0.0;
+
+        static ScriptControl _instance;
 
 		#endregion
 
@@ -103,7 +96,6 @@ namespace Sumerics.Controls
 
 		public ScriptControl()
 		{
-            instance = this;
 			InitializeComponent();
 			AddLeft();
 			AddRight();
@@ -116,10 +108,7 @@ namespace Sumerics.Controls
 
         public static ScriptControl Instance
         {
-            get
-            {
-                return instance ?? new ScriptControl();
-            }
+            get { return _instance ?? (_instance = new ScriptControl()); }
         }
 
 		public ObservableCollection<string> SensorFunctions
@@ -176,8 +165,8 @@ namespace Sumerics.Controls
 
         public void Shift(ScriptElement scriptElement, double dx, double dy)
         {
-            var left = dx / scale + Canvas.GetLeft(scriptElement);
-            var top = dy / scale + Canvas.GetTop(scriptElement);
+            var left = dx / _scale + Canvas.GetLeft(scriptElement);
+            var top = dy / _scale + Canvas.GetTop(scriptElement);
             Canvas.SetLeft(scriptElement, left);
             Canvas.SetTop(scriptElement, top);
         }
@@ -190,21 +179,21 @@ namespace Sumerics.Controls
 			ellipse.Height = 20;
 			TopContainer.Children.Add(ellipse);
 
-			Canvas.SetLeft(ellipse, 150 + 40 * inputNodes.Count);
+			Canvas.SetLeft(ellipse, 150 + 40 * _inputNodes.Count);
 			Canvas.SetBottom(ellipse, ellipse.Height);
-			inputNodes.Add(ctrl);
+			_inputNodes.Add(ctrl);
 
 			DeleteInputButton.IsEnabled = true;
-			AddInputButton.IsEnabled = inputNodes.Count < 8;
+			AddInputButton.IsEnabled = _inputNodes.Count < 8;
 		}
 
 		void RemoveInput()
 		{
-			var ctrl = inputNodes[inputNodes.Count - 1];
+			var ctrl = _inputNodes[_inputNodes.Count - 1];
 			TopContainer.Children.Remove(ctrl.Control);
-			inputNodes.RemoveAt(inputNodes.Count - 1);
+			_inputNodes.RemoveAt(_inputNodes.Count - 1);
 
-			DeleteInputButton.IsEnabled = inputNodes.Count > 0;
+			DeleteInputButton.IsEnabled = _inputNodes.Count > 0;
 			AddInputButton.IsEnabled = true;
 		}
 
@@ -216,80 +205,92 @@ namespace Sumerics.Controls
 			ellipse.Height = 20;
 			TopContainer.Children.Add(ellipse);
 
-			Canvas.SetRight(ellipse, 150 + 40 * inputNodes.Count);
+			Canvas.SetRight(ellipse, 150 + 40 * _inputNodes.Count);
 			Canvas.SetBottom(ellipse, ellipse.Height);
-			outputNodes.Add(ctrl);
+			_outputNodes.Add(ctrl);
 		}
 
 		#region Add Left and Right
 
 		void AddLeft()
 		{
-			var _sensor = new ScriptElement();
-			_sensor.ScriptHost = new SensorScriptObject();
-			var _function = new ScriptElement();
-			_function.ScriptHost = new FunctionScriptObject();
-			var _plot = new ScriptElement();
-			_plot.ScriptHost = new PlotScriptObject();
-			var _operator = new ScriptElement();
-			_operator.ScriptHost = new OperatorScriptObject();
+            var snsr = new ScriptElement
+            {
+                ScriptHost = new SensorScriptObject()
+            };
+            var func = new ScriptElement
+            {
+                ScriptHost = new FunctionScriptObject()
+            };
+            var plot = new ScriptElement
+            {
+                ScriptHost = new PlotScriptObject()
+            };
+            var oprt = new ScriptElement
+            {
+                ScriptHost = new OperatorScriptObject()
+            };
 
-			var list = new[] { _sensor, _function, _plot, _operator };
-			var pos = EL_OFFSET;
+            var list = new[] { snsr, func, plot, oprt };
+			var pos = ElementOffset;
 
 			foreach(var el in list)
 			{
 				Add(LeftContainer, 5, pos, el);
-				pos += EL_HEIGHT + EL_OFFSET;
+				pos += ElementHeight + ElementOffset;
 			}
 		}
 
 		void AddRight()
 		{
-			var _statement = new ScriptElement();
-			_statement.ScriptHost = new StatementScriptObject();
-			var _conditions = new ScriptElement();
-			_conditions.ScriptHost = new ConditionScriptObject();
+            var stmt = new ScriptElement
+            {
+                ScriptHost = new StatementScriptObject()
+            };
+            var cnds = new ScriptElement
+            {
+                ScriptHost = new ConditionScriptObject()
+            };
 
-			var list = new[] { _statement, _conditions };
-			var pos = EL_OFFSET;
+			var list = new[] { stmt, cnds };
+			var pos = ElementOffset;
 
 			foreach (var el in list)
 			{
 				Add(RightContainer, 5, pos, el);
-				pos += EL_HEIGHT + EL_OFFSET;
+				pos += ElementHeight + ElementOffset;
 			}
 		}
 
 		#endregion
 
-		void Add(Canvas container, int left, int top, ScriptElement element)
+        void Add(Canvas container, Int32 left, Int32 top, ScriptElement element)
 		{
 			container.Children.Add(element);
 			Canvas.SetLeft(element, left);
 			Canvas.SetTop(element, top);
 		}
 
-		void AddInputButtonClick(object sender, RoutedEventArgs e)
+		void AddInputButtonClick(Object sender, RoutedEventArgs e)
 		{
 			AddInput();
 		}
 
-		void DeleteInputButtonClick(object sender, RoutedEventArgs e)
+        void DeleteInputButtonClick(Object sender, RoutedEventArgs e)
 		{
 			RemoveInput();
 		}
 
-		void ToggleConnectorsButtonClick(object sender, RoutedEventArgs e)
+        void ToggleConnectorsButtonClick(Object sender, RoutedEventArgs e)
 		{
 
 		}
 
-		void CenterButtonClick(object sender, RoutedEventArgs e)
+        void CenterButtonClick(Object sender, RoutedEventArgs e)
 		{
-			scale = 1.0;
-			translateX = 0.0;
-			translateY = 0.0;
+			_scale = 1.0;
+			_translateX = 0.0;
+			_translateY = 0.0;
 			ApplyTransformations();
 		}
 
@@ -297,23 +298,25 @@ namespace Sumerics.Controls
 		{
 			var width = CenterContainer.ActualWidth;
 			var height = CenterContainer.ActualHeight;
-			var tr = new TranslateTransform(translateX, translateY);
-			var tg = new ScaleTransform(scale, scale, width / 2, height / 2);
+			var tr = new TranslateTransform(_translateX, _translateY);
+			var tg = new ScaleTransform(_scale, _scale, width / 2, height / 2);
 			CenterContainer.LayoutTransform = tg;
 
-			foreach (UIElement child in CenterContainer.Children)
-				child.RenderTransform = tr;
+            foreach (UIElement child in CenterContainer.Children)
+            {
+                child.RenderTransform = tr;
+            }
 		}
 
-		void MagnifyInButtonClick(object sender, RoutedEventArgs e)
+        void MagnifyInButtonClick(Object sender, RoutedEventArgs e)
 		{
-			scale *= 1.1;
+			_scale *= 1.1;
 			ApplyTransformations();
 		}
 
-		void MagnifyOutButtonClick(object sender, RoutedEventArgs e)
+        void MagnifyOutButtonClick(Object sender, RoutedEventArgs e)
 		{
-			scale /= 1.1;
+			_scale /= 1.1;
 			ApplyTransformations();
 		}
 
