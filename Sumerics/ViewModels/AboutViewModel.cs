b7a49d2ba12392f12ae7e270765548b86194740a
Florian Rappl <rappl.florian@gmail.com>
2016-02-27 18:40:53 +0100

    Wired to GPS device

--- Sumerics/ViewModels/AboutViewModel.cs
+++ Sumerics/ViewModels/AboutViewModel.cs
@@ -1,28 +1,46 @@
 ﻿namespace Sumerics.ViewModels
 {
     using System;
+    using YAMP.Sensors.Devices;
 
     sealed class AboutViewModel : BaseViewModel, IDisposable
     {
-        static readonly String _message = "Your current position is {0}° {1}° with heading {2}° north.";
+        static readonly String Message = "Your current position is {0}° {1}° with heading {2}° north.";
 
         readonly SumericsInfo _info;
-
-        Int32 _longitude;
-        Int32 _latitude;
-        Int32 _compass;
-
-        //readonly GPSFunction _gps;
-        //readonly CompFunction _cmp;
+        readonly Gps _gps;
+        readonly Compass _compass;
+        readonly ClientPosition _client;
+        String _position;
 
         public AboutViewModel()
         {
             _info = SumericsInfo.FromCurrentAssembly();
+            _gps = new Gps();
+            _compass = new Compass();
+            _client = new ClientPosition
+            {
+                Direction = _compass.CurrentHeading.Magnetic,
+                Latitude = _gps.CurrentLocation.Latitude,
+                Longitude = _gps.CurrentLocation.Longitude
+            };
 
-            //_gps = new GPSFunction();
-            //_gps.ReadingChanged += OnSensorChanged;
-            //_cmp = new CompFunction();
-            //_cmp.ReadingChanged += OnSensorChanged;
+            _gps.Changed += PositionChanged;
+            _compass.Changed += DirectionChanged;
+            _position = _client.ToString();
+        }
+
+        void DirectionChanged(Object sender, CompassEventArgs e)
+        {
+            _client.Direction = e.Value.Magnetic;
+            Position = _client.ToString();
+        }
+
+        void PositionChanged(Object sender, GpsEventArgs e)
+        {
+            _client.Longitude = e.Value.Longitude;
+            _client.Latitude = e.Value.Latitude;
+            Position = _client.ToString();
         }
 
         public String Copyright
@@ -37,21 +55,26 @@
 
         public String Position
         {
-            get { return String.Format(_message, _longitude, _latitude, _compass); }
-        }
-
-        void OnSensorChanged(Object sender, Object e)
-        {
-            //_longitude = (int)GPSFunction.Longitude;
-            //_latitude = (int)GPSFunction.Latitude;
-            //_compass = (int)CompFunction.HeadingMagneticNorth;
-            App.Current.Dispatcher.Invoke(() => RaisePropertyChanged("Position"));
+            get { return _position; }
+            private set { _position = value; RaisePropertyChanged(); }
         }
 
         void IDisposable.Dispose()
         {
-            //_gps.ReadingChanged -= OnSensorChanged;
-            //_cmp.ReadingChanged -= OnSensorChanged;
+            _gps.Changed -= PositionChanged;
+            _compass.Changed -= DirectionChanged;
+        }
+
+        sealed class ClientPosition
+        {
+            public Double Longitude;
+            public Double Latitude;
+            public Double Direction;
+
+            public override String ToString()
+            {
+                return String.Format(Message, Longitude.ToString("0"), Latitude.ToString("0"), Direction.ToString("0"));
+            }
         }
     }
 }
