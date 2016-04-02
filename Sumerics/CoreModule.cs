ceb332f1805f9e19a1b41363aba27cec33f73f8a
Florian Rappl <rappl.florian@gmail.com>
2016-04-02 16:09:22 +0200

    Register instances

--- Sumerics/CoreModule.cs
+++ Sumerics/CoreModule.cs
@@ -11,19 +11,14 @@
             var app = components.Get<IApplication>();
             var logger = new FileLogger();
             var console = new ConsoleProxy();
-            var tabs = new TabProxy();
-            var visualizer = new VisualizerProxy(console);
-            var kernel = new Kernel(logger);
-            var dialogs = new DialogManager(app);
-            var settings = new SettingsProxy(Settings.Default);
 
             components.Register<ILogger>(logger);
             components.Register<IConsole>(console);
-            components.Register<ITabs>(tabs);
-            components.Register<IVisualizer>(visualizer);
-            components.Register<IKernel>(kernel);
-            components.Register<IDialogManager>(dialogs);
-            components.Register<ISettings>(settings);
+            components.Register<ITabs>(new TabProxy());
+            components.Register<IVisualizer>(new VisualizerProxy(console));
+            components.Register<IKernel>(new Kernel(logger));
+            components.Register<IDialogManager>(new DialogManager(app));
+            components.Register<ISettings>(new SettingsProxy(Settings.Default));
         }
     }
 }
