4a5fc37072f9f98559c2436646af4df4340bdc12
Florian Rappl <rappl.florian@gmail.com>
2016-04-21 00:42:52 +0200

    Changed the product name

--- source/Sumerics/Properties/AssemblyInfo.cs
+++ source/Sumerics/Properties/AssemblyInfo.cs
@@ -6,7 +6,7 @@ using System.Windows;
 [assembly: AssemblyDescription("Sensor Numerics for Intel Ultrabooks™ combines the power of numerical computations with the ability to perform experiments with the Intel Ultrabook™ over the integrated sensors.")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Florian Rappl")]
-[assembly: AssemblyProduct("Sensor Numerics for Intel Ultrabooks™")]
+[assembly: AssemblyProduct("Sumerics")]
 [assembly: AssemblyCopyright("Copyright © 2012 - 2016")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
