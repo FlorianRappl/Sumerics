050e1cd1b73856bf92054b2bb3b8b417f929535e
Florian Rappl <rappl.florian@gmail.com>
2016-05-07 17:13:34 +0200

    Share common product info

---
+++ source/ProductInfo.cs
@@ -0,0 +1,9 @@
+using System.Reflection;
+
+[assembly: AssemblyDescription("Sensor Numerics for Intel Ultrabooks™ combines the power of numerical computations with the ability to perform experiments with the Intel Ultrabook™ over the integrated sensors.")]
+[assembly: AssemblyCompany("Florian Rappl")]
+[assembly: AssemblyProduct("Sumerics")]
+[assembly: AssemblyCopyright("Copyright © 2012 - 2016")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+[assembly: AssemblyVersion("2.0.0.*")]
--- source/Sumerics/Properties/AssemblyInfo.cs
+++ source/Sumerics/Properties/AssemblyInfo.cs
@@ -3,13 +3,6 @@ using System.Runtime.InteropServices;
 using System.Windows;
 
 [assembly: AssemblyTitle("Sumerics")]
-[assembly: AssemblyDescription("Sensor Numerics for Intel Ultrabooks™ combines the power of numerical computations with the ability to perform experiments with the Intel Ultrabook™ over the integrated sensors.")]
-[assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("Florian Rappl")]
-[assembly: AssemblyProduct("Sumerics")]
-[assembly: AssemblyCopyright("Copyright © 2012 - 2016")]
-[assembly: AssemblyTrademark("")]
-[assembly: AssemblyCulture("")]
-[assembly: ComVisible(false)]
 [assembly: ThemeInfo(ResourceDictionaryLocation.None, ResourceDictionaryLocation.SourceAssembly)]
-[assembly: AssemblyVersion("2.0.0.*")]
+[assembly: AssemblyConfiguration("")]
+[assembly: ComVisible(false)]
